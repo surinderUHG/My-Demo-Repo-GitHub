ba33157aa56ba4dc9c292499159821c5bd5cb6d0
unknown <ssin138@LH7U0CND439CH1M.ms.ds.uhc.com>
2015-01-11 10:48:19 -0500

    First commit

---
+++ META-INF/src/main/java/com/learning/EmailAddress.java
@@ -0,0 +1,27 @@
+package com.learning;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "emailAddress")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class EmailAddress {
+	
+	private String type;
+	private String address;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
